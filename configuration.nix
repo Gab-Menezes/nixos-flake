43c1340643b2755ec90e87c32d2cdf4a9c4e7b7a
Gabriel <gaha204@hotmail.com>
2024-09-27 01:41:50 -0300

    change timezone

--- configuration.nix
+++ configuration.nix
@@ -36,6 +36,8 @@
 
   programs.nix-ld.enable = true;
 
+  time.timeZone = "America/Sao_Paulo";
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
