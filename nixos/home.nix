26d044fe224b4ea99dea20e3515ed3e752340ef5
Gabriel <gaha204@hotmail.com>
2024-12-21 15:56:46 -0300

    initial nixos (broken)

---
+++ nixos/home.nix
@@ -0,0 +1,201 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  nixpkgs.config.allowUnfree = true;
+  dconf.settings."org/gnome/desktop/wm/preferences" = {
+    button-layout = "appmenu:minimize,maximize,close";
+    num-workspaces = 1;
+  };
+  dconf.settings."org/gnome/desktop/interface" = {
+    color-scheme = "prefer-dark";
+    enable-hot-corners = false;
+  };
+  dconf.settings."org/gnome/shell/keybindings" = {
+    show-screenshot-ui = [
+      "<Shift><Super>s"
+    ];
+  };
+  dconf.settings."org/gnome/mutter" = {
+    edge-tiling = true;
+  };
+  dconf.settings."org/gnome/desktop/session" = {
+    idle-delay = "uint32 0";
+  };
+  dconf.settings."org/gnome/settings-daemon/plugins/color" = {
+    night-light-schedule-automatic = false;
+  };
+  dconf.settings."org/gnome/settings-daemon/plugins/power" = {
+    sleep-inactive-ac-type = "nothing";
+  };
+
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "gabriel";
+  home.homeDirectory = "/home/gabriel";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+
+    wget
+    keychain
+    age
+    sops
+
+    jq
+    lf
+    bottom
+    ripgrep
+    bat
+    vim
+    file
+    patchelf
+
+    nil
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  home.sessionVariables = {
+    EDITOR = "vim";
+    MYVAR = "abc";
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/gabriel/etc/profile.d/hm-session-vars.sh
+  #
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs.bash = {
+    enable = true;
+    historyControl = [ "ignoredups" "erasedups" ];
+    shellAliases = {
+      ls = "eza";
+      ll = "eza -alBhog";
+      grep = "rg";
+      h = "history";
+      find = "fd";
+      cdsw = "cd ~/dev/nixos-flake";
+      csw = "code ~/dev/nixos-flake";
+      sw = "sudo nixos-rebuild switch --flake ~/dev/nixos-flake/nixos";
+      rcargo = "nix run nixpkgs#cargo -- ";
+    };
+    #bashrcExtra = import ./configs/bashrc.nix;
+  };
+
+
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      vscode-icons-team.vscode-icons
+      mskelton.one-dark-theme
+      usernamehw.errorlens
+      vadimcn.vscode-lldb
+      fill-labs.dependi
+      mkhl.direnv
+      tamasfe.even-better-toml
+      mhutchie.git-graph
+      oderwat.indent-rainbow
+      jnoortheen.nix-ide
+      rust-lang.rust-analyzer
+      gruntfuggly.todo-tree
+    ];
+    userSettings = {
+      "workbench.colorTheme" = "One Dark";
+      "workbench.iconTheme" = "vscode-icons";
+      "files.autoSave" = "afterDelay";
+
+      "rust-analyzer.inlayHints.parameterHints.enable" = false;
+      "rust-analyzer.inlayHints.chainingHints.enable" = false;
+      "rust-analyzer.inlayHints.typeHints.enable" = false;
+      "rust-analyzer.debug.engine" = "vadimcn.vscode-lldb";
+
+      "git.confirmSync" = false;
+      "nix.enableLanguageServer" = true;
+      "window.zoomLevel" = 2;
+    };
+  };
+  programs.git = {
+    enable = true;
+    userEmail = "gaha204@hotmail.com";
+    userName = "Gabriel";
+  };
+  programs.fastfetch = {
+    enable = true;
+    settings = import ../configs/fastfetch.nix;
+  };
+  programs.htop = {
+    enable = true;
+    settings = import ../configs/htop.nix;
+  };
+  programs.eza = {
+    enable = true;
+    git = true;
+  };
+  programs.fd = {
+    enable = true;
+  };
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+  programs.nix-index = {
+    enable = true;
+  };
+
+
+  nix.registry.self.flake = inputs.self;
+}
