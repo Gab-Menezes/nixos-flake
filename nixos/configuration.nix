ea229cf0f92145e6ce1af4a55a772822af95b4ed
Gabriel <gaha204@hotmail.com>
2024-12-23 23:00:59 -0300

    update kernel

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -195,4 +195,5 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
 
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 }
