fd78566582a88577fee53f50f050670d9df814fd
Gabriel <gaha204@hotmail.com>
2025-03-04 02:13:36 -0300

    rename alias

--- nixos/home.nix
+++ nixos/home.nix
@@ -149,7 +149,7 @@
       cdsw = "cd ~/dev/nixos-flake";
       csw = "code ~/dev/nixos-flake";
       sw = "sudo nixos-rebuild switch --flake ~/dev/nixos-flake#nixos";
-      sb = "sudo nixos-rebuild boot --flake ~/dev/nixos-flake#nixos";
+      bw = "sudo nixos-rebuild boot --flake ~/dev/nixos-flake#nixos";
       rcargo = "nix run nixpkgs#cargo -- ";
       preperf = "echo 65536 | sudo tee /proc/sys/kernel/perf_event_max_sample_rate && echo -1 | sudo tee /proc/sys/kernel/perf_event_paranoid && echo 0 | sudo tee /proc/sys/kernel/kptr_restrict";
     };
@@ -179,7 +179,7 @@
       cdsw = "cd ~/dev/nixos-flake";
       csw = "code ~/dev/nixos-flake";
       sw = "sudo nixos-rebuild switch --flake ~/dev/nixos-flake#nixos";
-      sb = "sudo nixos-rebuild boot --flake ~/dev/nixos-flake#nixos";
+      bw = "sudo nixos-rebuild boot --flake ~/dev/nixos-flake#nixos";
       rcargo = "nix run nixpkgs#cargo -- ";
       preperf = "echo 65536 | sudo tee /proc/sys/kernel/perf_event_max_sample_rate && echo -1 | sudo tee /proc/sys/kernel/perf_event_paranoid && echo 0 | sudo tee /proc/sys/kernel/kptr_restrict";
     };
