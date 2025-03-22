78a3e12087ff879971a50466cf916a4ba5176d07
Gabriel Menezes <gaha204@hotmail.com>
2025-03-22 11:56:23 -0700

    add mac config

---
+++ mac/configuration.nix
@@ -0,0 +1,60 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, inputs, pc, ... }:
+
+{
+  nixpkgs.hostPlatform = pc.system;
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.cudaSupport = true;
+  
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+
+  # imports = [
+  #   inputs.home-manager.darwinModules.default
+  #   ../secrets/sops.nix
+  # ];
+
+  # home-manager = {
+  #   extraSpecialArgs = { inherit inputs; };
+  #   users = {
+  #     gabriel = import ./home.nix;
+  #   };
+  # };
+
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-emoji
+    liberation_ttf
+    fira-code
+    fira-code-symbols
+    mplus-outline-fonts.githubRelease
+    dina-font
+    proggyfonts
+    font-awesome
+    icomoon-feather
+  ] 
+  ++
+  builtins.filter 
+    lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
+
+  networking.hostName = pc.host;
+
+  users.users.${pc.user} = {
+    shell = pkgs.zsh;
+  };
+
+  programs.zsh.enable = true;
+  # programs.ssh.startAgent = true;
+
+  environment.systemPackages = with pkgs; [];
+
+  system.stateVersion = 6;
+}
